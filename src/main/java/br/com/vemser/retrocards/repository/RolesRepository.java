7662829e3acfb69ad5a36a2a18af9e3a9d5cb709
Willian Valentim <williann.valentim@gmail.com>
2022-08-13 18:43:55 -0300

    [feature] Refatoração da RolesRepository

--- src/main/java/br/com/vemser/retrocards/repository/RolesRepository.java
+++ src/main/java/br/com/vemser/retrocards/repository/RolesRepository.java
@@ -2,6 +2,8 @@ package br.com.vemser.retrocards.repository;
 
 import br.com.vemser.retrocards.entity.RolesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RolesRepository extends JpaRepository<RolesEntity, Integer> {
 }
