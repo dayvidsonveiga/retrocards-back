1a9fe7dd5be27d0db27b515fe84ce5c278cb5a59
dayvidsonveiga <dayvidsonveiga@gmail.com>
2022-08-13 03:09:20 -0300

    Feat: Estruturado base, segurança e swagger.

---
+++ src/test/java/br/com/vemser/retrocards/RetrocardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vemser.retrocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrocardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
