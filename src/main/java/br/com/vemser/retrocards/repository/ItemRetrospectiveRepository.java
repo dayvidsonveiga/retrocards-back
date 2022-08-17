96cd757b9f2ae2ddbbc425960794326fcb673111
Willian Valentim <williann.valentim@gmail.com>
2022-08-17 17:07:22 -0300

    [feature] Adicionado @Repository nas interfaces

--- src/main/java/br/com/vemser/retrocards/repository/ItemRetrospectiveRepository.java
+++ src/main/java/br/com/vemser/retrocards/repository/ItemRetrospectiveRepository.java
@@ -2,9 +2,11 @@ package br.com.vemser.retrocards.repository;
 
 import br.com.vemser.retrocards.entity.ItemRetrospectiveEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ItemRetrospectiveRepository extends JpaRepository<ItemRetrospectiveEntity, Integer> {
 
     List<ItemRetrospectiveEntity> findAllByRetrospective_IdRetrospective(Integer idRestrospective);
--- src/main/java/br/com/vemser/retrocards/repository/KudoBoxRepository.java
+++ src/main/java/br/com/vemser/retrocards/repository/KudoBoxRepository.java
@@ -4,7 +4,9 @@ import br.com.vemser.retrocards.entity.KudoBoxEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KudoBoxRepository extends JpaRepository<KudoBoxEntity, Integer> {
 
     Page<KudoBoxEntity> findAllBySprint_IdSprint(Integer idSprint, Pageable pageable);
