90751c1a10bdf6b94a242ea657b8657d023b9649
dayvidsonveiga <dayvidsonveiga@gmail.com>
2022-08-19 15:28:08 -0300

    Fix: Retorno do Kudobox

--- src/main/java/br/com/vemser/retrocards/dto/kudo/kudobox/KudoBoxWithCountOfItensDTO.java
+++ src/main/java/br/com/vemser/retrocards/dto/kudo/kudobox/KudoBoxWithCountOfItensDTO.java
@@ -1,5 +1,6 @@
 package br.com.vemser.retrocards.dto.kudo.kudobox;
 
+import br.com.vemser.retrocards.enums.KudoStatus;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -20,7 +21,7 @@ public class KudoBoxWithCountOfItensDTO {
     private LocalDateTime endDate;
 
     @Schema(description = "Status da Kudo box.")
-    private String status;
+    private KudoStatus status;
 
     @Schema(description = "Quantidade de itens cadastrados na kudobox.")
     private Integer numberOfItens;
