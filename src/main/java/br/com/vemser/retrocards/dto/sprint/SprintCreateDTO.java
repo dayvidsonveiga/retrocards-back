775b20c8c9dd35464cc6860ec2d4245dc1031f04
Willian Valentim <williann.valentim@gmail.com>
2022-08-19 13:50:45 -0300

    [feature] Adição das mensagens nas validações.

--- src/main/java/br/com/vemser/retrocards/dto/sprint/SprintCreateDTO.java
+++ src/main/java/br/com/vemser/retrocards/dto/sprint/SprintCreateDTO.java
@@ -3,6 +3,7 @@ package br.com.vemser.retrocards.dto.sprint;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -12,16 +13,17 @@ import java.time.LocalDate;
 public class SprintCreateDTO {
 
     @Schema(description = "Título da sprint.")
-    @NotBlank
-    @Size(min = 3, max = 60)
+    @NotBlank(message = "O título da sprint não pode ser vazio/nulo.")
+    @Size(message = "O título da sprint deve conter no mínimo 3 e no máximo 60 caracteres.", min = 3, max = 60)
     private String title;
 
     @Schema(description = "Data de início da sprint.")
-    @NotNull
+    @NotNull(message = "A data de início da sprint não pode ser vazia/nula.")
+    @FutureOrPresent
     private LocalDate startDate;
 
     @Schema(description = "Data de término da sprint.")
-    @NotNull
+    @NotNull(message = "A data de término da sprint não pode ser vazia/nula.")
+    @FutureOrPresent
     private LocalDate endDate;
-
 }
