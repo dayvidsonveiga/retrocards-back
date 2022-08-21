134f8d73dd428f5cfaf83b714e52bb46744c1a51
dayvidsonveiga <dayvidsonveiga@gmail.com>
2022-08-21 17:22:43 -0300

    Fix: Adicionado Status no List da Sprint

--- src/main/java/br/com/vemser/retrocards/dto/sprint/SprintCheckDTO.java
+++ src/main/java/br/com/vemser/retrocards/dto/sprint/SprintCheckDTO.java
@@ -1,5 +1,6 @@
 package br.com.vemser.retrocards.dto.sprint;
 
+import br.com.vemser.retrocards.enums.SprintStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -19,6 +20,9 @@ public class SprintCheckDTO {
 
     @Schema(description = "Data de término da sprint.")
     private LocalDateTime endDate;
+
+    @Schema(description = "Status da Sprint")
+    private SprintStatus status;
 
     @Schema(description = "Verifica se há alguma retrospectiva em andamento.")
     private Boolean verifyRetrospective;
