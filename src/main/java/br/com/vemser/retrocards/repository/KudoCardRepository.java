cc0d79fe4ce308e51933aa8cef8c94909ef295fb
Willian Valentim <williann.valentim@gmail.com>
2022-08-22 13:03:16 -0300

    [feature] Remoção de métodos não utilizados.

--- src/main/java/br/com/vemser/retrocards/repository/KudoCardRepository.java
+++ src/main/java/br/com/vemser/retrocards/repository/KudoCardRepository.java
@@ -4,18 +4,12 @@ import br.com.vemser.retrocards.entity.KudoCardEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KudoCardRepository extends JpaRepository<KudoCardEntity, Integer> {
 
     Page<KudoCardEntity> findAllByKudobox_IdKudoBox(Integer idIdKudoBox, Pageable pageable);
-
-    @Query(value = "    SELECT k " +
-            "             FROM kudo_cards k " +
-            "         ORDER BY k.createDate ASC")
-    Page<KudoCardEntity> findAllByCreateDateOrderByCreateDate(Pageable pageable);
 
     Integer countAllByKudobox_IdKudoBox(Integer idKudoBox);
 }
--- src/main/java/br/com/vemser/retrocards/repository/SprintRepository.java
+++ src/main/java/br/com/vemser/retrocards/repository/SprintRepository.java
@@ -2,21 +2,13 @@ package br.com.vemser.retrocards.repository;
 
 import br.com.vemser.retrocards.entity.SprintEntity;
 import br.com.vemser.retrocards.enums.SprintStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SprintRepository extends JpaRepository<SprintEntity, Integer> {
-
-    @Query(value = "    SELECT s " +
-            "             FROM sprints s " +
-            "         ORDER BY s.endDate DESC")
-    Page<SprintEntity> listByEndDateOrderedDesc(Pageable pageable);
 
     List<SprintEntity> findAllByStatusIs(SprintStatus status);
 }
